6b34a9b71fe2648b97b65123650a1fd1a20d453a
CarlosYang <mrcarlosyang@gmail.com>
2018-03-06 21:10:14 -0500

    feat:Add main UI

---
+++ app/src/main/java/com/me/cl/letschat/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.me.cl.letschat
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by CL on 3/6/18.
+ */
+open class BaseActivity: AppCompatActivity() {
+}
--- app/src/main/java/com/me/cl/letschat/MainActivity.kt
+++ app/src/main/java/com/me/cl/letschat/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.me.cl.letschat
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
